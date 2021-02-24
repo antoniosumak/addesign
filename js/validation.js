8ed3cb58644cece25baa343bdcb384f8ed1572a0
Antonio Sumak <sumakantonio@gmail.com>
2021-02-24 02:03:43 +0100

    init

--- js/validation.js
+++ js/validation.js
@@ -3,6 +3,7 @@ var email = document.getElementById("email");
 var telefon = document.getElementById("telefon");
 var poruka = document.getElementById("poruka");
 var gumb = document.getElementById("my-form-button");
+const inputi = document.querySelector("form");
 
 gumb.addEventListener("click", function () {
   if (imeprezime.value == "") {
@@ -18,3 +19,21 @@ gumb.addEventListener("click", function () {
     document.getElementById("greskaporuka").style.visibility = "visible";
   } else document.getElementById("greskaporuka").style.visibility = "hidden";
 });
+
+function salji() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "antonio2.sumak@gmail.com",
+    Password: "2YDYTK2M",
+    To: "sumakantonio@gmail.com",
+    From: inputi.elements["emailadresa"].value,
+    Subject: "This is the subject",
+    Body:
+      inputi.elements["imeprezime"].value +
+      "<br>" +
+      inputi.elements["brojmobitela"] +
+      "<br>" +
+      inputi.elements["poruka"] +
+      "<br>",
+  }).then((message) => alert(message));
+}
